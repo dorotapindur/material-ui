36d075bd433dd7ef002d76ff963aa3b64f629c64
Dorota Pindur <dpindur@wp.pl>
2021-01-01 20:41:34 +0100

    add editing content and like changing color

--- src/App.js
+++ src/App.js
@@ -1,57 +1,71 @@
+import React from "react";
 import './App.css';
 import { CssBaseline, Button, IconButton, Icon, Container, Card, CardActions, CardContent, Typography, CardHeader, Avatar, CardMedia } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
 
-
-const useStyles = makeStyles({
-  media: {
-    height: 200
-  },
-  readMore: {
-    marginLeft: "auto",
-    color: "purple"
-  },
-  avatar: {
-    backgroundColor: "purple"
-  },
-  cardMargin: {
-    margin: 20
+class ArticleCard extends React.Component {
+  state = {
+    isLiked: false
   }
-})
-
-function ArticleCard(props) {
-  const classes = useStyles();
-  const author = "John Smith";
-  const imageUrl = "https://picsum.photos/600/400";
-  const arcticleTitle = "Clickbait title";
-  const articleFirstSentence = "First sentence - something very interesting..."
-  return (
-    <Card className={classes.cardMargin}>
-      <CardHeader
-        avatar={<Avatar className={classes.avatar}>JD</Avatar>}
-        title={author}
-        subheader="written 01-01-2021"
-      />
-      <CardMedia
-        className={classes.media}
-        component="img"
-        image={imageUrl}
-      />
-      <CardContent>
-        <Typography variant="h6" component="h2">{arcticleTitle}</Typography>
-        <Typography variant="body2">{articleFirstSentence}</Typography>
-      </CardContent>
-      <CardActions>
-        <IconButton>
-          <Icon>favorite</Icon>
-        </IconButton>
-        <IconButton>
-          <Icon>share</Icon>
-        </IconButton>
-        <Button className={classes.readMore} variant="outlined" color="primary">Read more</Button>
-      </CardActions>
-    </Card>
-  )
+  handleLike = (event) => {
+    this.toggleLike();
+  }
+  toggleLike = () => {
+    this.setState(
+      function(prevState) {
+        const isLiked = !prevState.isLiked;
+        return {
+          isLiked
+        }
+      }
+    )
+    console.log(this.state.isLiked);
+  }
+  render() {
+    const author = "John Smith";
+    const imageUrl = "https://picsum.photos/id/1078/600/400";
+    const arcticleTitle = "Clickbait title";
+    const articleFirstSentence = "First sentence - something very interesting..."
+    const { isLiked } = this.state;
+    let likeStyle = {};
+    if (isLiked) {
+      likeStyle = {
+        backgroundColor: "red"
+      }
+    } else {
+      likeStyle = {}
+    }
+    return (
+      <Card style={{margin: 20}}>
+        <CardHeader
+          avatar={<Avatar style={{backgroundColor: "purple"
+            }}>JD</Avatar>}
+          title={author}
+          subheader="written 01-01-2021"
+        />
+        <CardMedia
+          style={{height: 200}}
+          component="img"
+          image={imageUrl}
+        />
+        <CardContent>
+          <Typography variant="h6" component="h2">{arcticleTitle}</Typography>
+          <Typography variant="body2">{articleFirstSentence}</Typography>
+        </CardContent>
+        <CardActions>
+          <IconButton style={likeStyle} onClick={this.handleLike} >
+            <Icon>favorite</Icon>
+          </IconButton>
+          <IconButton>
+            <Icon>share</Icon>
+          </IconButton>
+          <Button
+            style={{marginLeft: "auto", color: "purple"}}
+            variant="outlined"
+            color="primary">Read more</Button>
+        </CardActions>
+      </Card>
+    )
+  }
 }
 
 function App() {
